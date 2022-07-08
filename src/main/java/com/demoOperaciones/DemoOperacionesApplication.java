30da3163b4a22beddcc8f747d7d3fb75ce9d412f
Marcos Tulisso <marcostulisso@hotmail.com>
2022-07-08 16:59:38 -0300

    version inicial

---
+++ src/main/java/com/demoOperaciones/DemoOperacionesApplication.java
@@ -0,0 +1,13 @@
+package com.demoOperaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOperacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoOperaciones/DemoOperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoOperaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOperacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
